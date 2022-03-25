4dd1a9855ab91fe812eda74943a0c23341b87a72
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-03-25 00:00:39 -0600

    first commit

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const path = require("path");
 require('dotenv').config();
 
 // Connect to db 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Mongodb connected.."))
+.catch(err => console.log(err));
 
 // Middleware functions 
 const ignoreFavicon = (req, res, next)=>{
