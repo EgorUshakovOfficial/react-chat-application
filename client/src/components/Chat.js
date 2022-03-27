77105581ee1492a47d714af1ec3ddea671b27292
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-03-27 01:16:29 -0600

    first commit

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -12,7 +12,7 @@ const Chat = ({ user, submitNewMessage, logout }) => {
     const [activeUsers, setActiveUsers] = useState([]); 
     useEffect(() => {
         // New connection 
-        const socket = io();
+        const socket = io("https://friends-book1.herokuapp.com/", {secure: true});
         setSocket(socket);
 
         // Active users
