d16b5f0b07a275537237d4fd31054c35cad4bd40
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-03-26 18:19:18 -0600

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo');
 const io = require("socket.io")(server, {
-    path: '/socket.io'
+    path: '/socket.io/'
 });
 
 const path = require("path"); 
