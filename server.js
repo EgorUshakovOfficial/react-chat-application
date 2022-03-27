f95857ecb93f4620309b7a51929b410d690d1199
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-03-27 11:25:24 -0600

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const auth = require('./auth/auth');
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo');
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {port: 443});
 const path = require("path");
 
 // Dotenv
